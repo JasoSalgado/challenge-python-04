d3d14e3aa5f1b5435db05269ac6a28d273d6d732
Jaso Salgado <jaso_98@hotmail.com>
2020-05-26 10:44:11 -0500

    Challenge solved

--- html_decorators.py
+++ html_decorators.py
@@ -1,28 +1,38 @@
+# coding: utf-8
 def div(func):
     # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f'<div>{func (*args, **kwargs) }</div>'
+
+    return wrapper
 
 
 def article(func):
     # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f'<article>{func (*args, **kwargs) }</article>'
+    
+    return wrapper
 
 
 def p(func):
     # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f'<p>{func (*args, **kwargs) }</p>'
+    
+    return wrapper
 
 
 # Here you must apply the decorators, uncomment this later
-# @div
-# @article
-# @p
+@div
+@article
+@p
 def saludo(nombre):
     return f'¡Hola {nombre}, ¿Cómo estás?'
 
 
 def run():
-    print(saludo('Jorge'))
+    print(saludo('Jorge Arturo Salgado Ordoñez'))
 
 
 if __name__ == '__main__':
